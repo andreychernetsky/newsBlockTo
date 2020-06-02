638a4c3468d151808ddda202c058c08102e99980
andric <rodomir3119841985@gmail.com>
2020-06-02 11:24:04 +0300

    second menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,32 +1,27 @@
-document.querySelector('.c-hamburger').addEventListener('click', function (e) {
-  e.preventDefault();
-  // this.classList.toggle('is-active');
-  if (this.classList.contains('is-active')) {
-    this.classList.remove('is-active');
-    document.querySelector('#menu').classList.remove('nav-active');
-    document.body.classList.remove('body-active');
-  } else {
-    this.classList.add('is-active');
-    document.querySelector('#menu').classList.add('nav-active');
-    document.body.classList.add('body-active');
+document.addEventListener('DOMContentLoaded', function() {
+  
+const btn = document.getElementById('menu-button'),
+  menuOpen = document.getElementById('menu-overlay');
+
+  let menu = document.querySelector('.menu-overlay');
+  let menubutton = document.querySelector('.menu-button');
+
+
+
+btn.addEventListener('click',toggle_visibility);
+  function toggle_visibility(){
+    if(menuOpen.style.display === 'flex') {
+      menuOpen.style.display ='none';
+    } else {
+      menuOpen.style.display ='flex';
+    }
+  }
+
+
+window.addEventListener('click',function(e){
+  if(e.target === menuOpen) {
+    menuOpen.style.display = "none";
   }
 });
 
-
-/*
-let burger = document.querySelector('.c-hamburger');
-let bodyB = document.querySelector('.body');
-let nav = document.querySelector('.nav');
-let timeout;
-
-nav.addEventListener('mouseout', function(){
-  clearTimeout(timeout);
-  timeout = setTimeout(()=>{
-    bodyB.classList.remove('is-active');
-  },2000);
-});
-
-burger.addEventListener('click', function () {
-bodyB.classList.toggle('is-active')
-});
-*/
+})();
