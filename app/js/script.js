8402efb9bd5a0effd343bd6391929fa7a7298cf3
andric <rodomir3119841985@gmail.com>
2020-06-05 10:31:21 +0300

    scroll and button up

--- app/js/script.js
+++ app/js/script.js
@@ -1,27 +1,84 @@
-document.addEventListener('DOMContentLoaded', function() {
-  
-const btn = document.getElementById('menu-button'),
-  menuOpen = document.getElementById('menu-overlay');
+/*
+{
+  const menubtn = document.querySelector('.menu-btn'),
+    menuOpen = document.getElementById('menu-overlay');
 
-  let menu = document.querySelector('.menu-overlay');
-  let menubutton = document.querySelector('.menu-button');
+  menubtn.onclick = function() {
+    menuOpen.style.display ='flex';
+    menuOpen.classList.toggle('active');
+    menubtn.classList.toggle('active');
+  };
 
-
-
-btn.addEventListener('click',toggle_visibility);
-  function toggle_visibility(){
-    if(menuOpen.style.display === 'flex') {
+  window.addEventListener('click', function (e) {
+    if (!menubtn.contains(e.target) && !menuOpen.contains(e.target)) {
+      menuOpen.classList.remove('active');
       menuOpen.style.display ='none';
-    } else {
-      menuOpen.style.display ='flex';
+      menubtn.classList.remove('active');
+    }
+  });
+}
+
+*/
+{
+  const menubtn = document.querySelector('.menu-btn');
+  const menuOpen = document.getElementById('menu-overlay');
+
+  menubtn.addEventListener('click',()=>{
+    menuOpen.style.display ='flex';
+    menuOpen.classList.toggle('active');
+    menubtn.classList.toggle('active');
+  });
+
+  window.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (!menubtn.contains(e.target) && !menuOpen.contains(e.target) || e.target === menuOpen) {
+      menuOpen.classList.remove('active');
+      menuOpen.style.display ='none';
+      menubtn.classList.remove('active');
+    }
+  });
+
+}
+
+// scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+//кнопка up
+{
+  function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('topButton__show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('topButton__show');
     }
   }
 
-
-window.addEventListener('click',function(e){
-  if(e.target === menuOpen) {
-    menuOpen.style.display = "none";
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
   }
-});
 
-})();
+  const goTopBtn = document.querySelector('.topButton');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+}
